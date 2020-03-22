8ea80d108a1653d3a1bcfe21b050ab81dbdc9b3e
nickchang1716 <62132705+nickchang1716@users.noreply.github.com>
2020-03-22 22:21:16 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,794 @@
+var breakfastInf = [{
+    "name": "滿福堡",
+    "calories": 259
+  },
+  {
+    "name": "青蔬滿福堡",
+    "calories": 261
+  },
+  {
+    "name": "豬肉滿福堡",
+    "calories": 278
+  },
+  {
+    "name": "火腿蛋堡",
+    "calories": 291
+  },
+  {
+    "name": "鬆餅(三片)",
+    "calories": 323
+  },
+  {
+    "name": "香鷄滿福堡",
+    "calories": 326
+  },
+  {
+    "name": "豬肉滿福堡加蛋",
+    "calories": 340
+  },
+  {
+    "name": "吉事蛋堡",
+    "calories": 370
+  },
+  {
+    "name": "豬肉蛋堡",
+    "calories": 374
+  },
+  {
+    "name": "經典大早餐",
+    "calories": 384
+  },
+  {
+    "name": "香鷄滿福堡加蛋",
+    "calories": 388
+  },
+  {
+    "name": "香鷄蛋堡",
+    "calories": 396
+  },
+  {
+    "name": "豬肉鬆餅",
+    "calories": 426
+  },
+  {
+    "name": "無敵豬肉滿福堡加蛋",
+    "calories": 442
+  },
+  {
+    "name": "金黃薯餅烤土司",
+    "calories": 565
+  },
+  {
+    "name": "火腿薯餅烤土司",
+    "calories": 586
+  },
+  {
+    "name": "黃金豬排烤土司",
+    "calories": 639
+  },
+  {
+    "name": "鷄塊鬆餅大早餐",
+    "calories": 694
+  },
+  {
+    "name": "脆鷄腿排烤土司",
+    "calories": 780
+  }
+];
+var normalInf = [{
+    "name": "一口咬麥麥鷄",
+    "calories": 87
+  },
+  {
+    "name": "麥克鷄塊(4塊)",
+    "calories": 193
+  },
+  {
+    "name": "義式烤鷄沙拉",
+    "calories": 244
+  },
+  {
+    "name": "漢堡",
+    "calories": 246
+  },
+  {
+    "name": "陽光鱈魚堡",
+    "calories": 248
+  },
+  {
+    "name": "麥克鷄塊(6塊)",
+    "calories": 290
+  },
+  {
+    "name": "吉事漢堡",
+    "calories": 295
+  },
+  {
+    "name": "麥脆鷄翅辣味",
+    "calories": 309
+  },
+  {
+    "name": "麥脆鷄翅原味",
+    "calories": 318
+  },
+  {
+    "name": "麥香魚",
+    "calories": 321
+  },
+  {
+    "name": "麥香鷄",
+    "calories": 356
+  },
+  {
+    "name": "凱撒脆鷄沙拉",
+    "calories": 370
+  },
+  {
+    "name": "嫩煎鷄腿堡",
+    "calories": 373
+  },
+  {
+    "name": "美式辣味香雞堡",
+    "calories": 373
+  },
+  {
+    "name": "千島黃金蝦堡",
+    "calories": 379
+  },
+  {
+    "name": "法式芥末香鷄堡",
+    "calories": 379
+  },
+  {
+    "name": "安格斯黑牛堡",
+    "calories": 416
+  },
+  {
+    "name": "黃金起司豬排堡",
+    "calories": 423
+  },
+  {
+    "name": "BLT嫩煎鷄腿堡",
+    "calories": 435
+  },
+  {
+    "name": "麥脆鷄腿辣味",
+    "calories": 436
+  },
+  {
+    "name": "麥脆鷄腿原味",
+    "calories": 444
+  },
+  {
+    "name": "雙層牛肉吉事堡",
+    "calories": 446
+  },
+  {
+    "name": "麥克鷄塊(10塊)",
+    "calories": 446
+  },
+  {
+    "name": "勁辣鷄腿堡",
+    "calories": 490
+  },
+  {
+    "name": "BLT安格斯黑牛堡",
+    "calories": 525
+  },
+  {
+    "name": "BLT辣脆鷄腿堡",
+    "calories": 533
+  },
+  {
+    "name": "大麥克",
+    "calories": 534
+  },
+  {
+    "name": "蕈菇安格斯黑牛堡",
+    "calories": 575
+  }
+];
+var snackInf = [{
+    "name": "四季沙拉(小)",
+    "calories": 31
+  },
+  {
+    "name": "四季沙拉(大)",
+    "calories": 35
+  },
+  {
+    "name": "水果袋-蘋果切片",
+    "calories": 36
+  },
+  {
+    "name": "玉米杯",
+    "calories": 86
+  },
+  {
+    "name": "薯餅",
+    "calories": 109
+  },
+  {
+    "name": "蛋捲冰淇淋",
+    "calories": 151
+  },
+  {
+    "name": "薯條(小包)",
+    "calories": 188
+  },
+  {
+    "name": "麥當勞搖搖樂鷄球",
+    "calories": 189
+  },
+  {
+    "name": "勁辣香鷄翅",
+    "calories": 235
+  },
+  {
+    "name": "蘋果派",
+    "calories": 240
+  },
+  {
+    "name": "酥嫩鷄翅",
+    "calories": 240
+  },
+  {
+    "name": "薯條(中包)",
+    "calories": 267
+  },
+  {
+    "name": "麥當勞搖搖樂鷄腿排",
+    "calories": 319
+  },
+  {
+    "name": "聖代(巧克力)",
+    "calories": 323
+  },
+  {
+    "name": "薯條(大包)",
+    "calories": 376
+  },
+  {
+    "name": "冰炫風(OREO)",
+    "calories": 379
+  }
+];
+var drinkInf = [{
+    "name": "熱紅茶",
+    "calories": 0
+  },
+  {
+    "name": "冰紅茶(無糖)",
+    "calories": 0
+  },
+  {
+    "name": "可口可樂ZERO(大)",
+    "calories": 0
+  },
+  {
+    "name": "可口可樂纖維+(大)",
+    "calories": 0
+  },
+  {
+    "name": "冰綠茶(原萃-無糖)(大)",
+    "calories": 6
+  },
+  {
+    "name": "熱奶茶",
+    "calories": 36
+  },
+  {
+    "name": "焦糖熱奶茶",
+    "calories": 87
+  },
+  {
+    "name": "玉米湯(大杯)",
+    "calories": 141
+  },
+  {
+    "name": "鮮乳",
+    "calories": 160
+  },
+  {
+    "name": "冰奶茶",
+    "calories": 171
+  },
+  {
+    "name": "柳橙汁(大)",
+    "calories": 203
+  },
+  {
+    "name": "冰阿薩姆鮮奶茶",
+    "calories": 228
+  },
+  {
+    "name": "冰紅茶(飛想茶-檸檬風味)(大)",
+    "calories": 233
+  },
+  {
+    "name": "冰阿薩姆咖啡鮮奶茶(大)",
+    "calories": 236
+  },
+  {
+    "name": "冰宇治抹茶鮮奶茶(大)",
+    "calories": 237
+  },
+  {
+    "name": "冰宇治抹茶咖啡鮮奶茶(大)",
+    "calories": 244
+  },
+  {
+    "name": "熱巧克力(大)",
+    "calories": 247
+  },
+  {
+    "name": "可口可樂(大)",
+    "calories": 297
+  },
+  {
+    "name": "阿薩姆咖啡鮮奶茶(大)",
+    "calories": 298
+  },
+  {
+    "name": "抹茶咖啡鮮奶茶(大)",
+    "calories": 303
+  },
+  {
+    "name": "雪碧(大)",
+    "calories": 326
+  },
+  {
+    "name": "阿薩姆鮮奶茶(大)",
+    "calories": 329
+  },
+  {
+    "name": "焦糖瑪琪朵(大)",
+    "calories": 313
+  },
+  {
+    "name": "抹茶鮮奶茶(大)",
+    "calories": 334
+  },
+  {
+    "name": "芒果萊姆冰沙",
+    "calories": 360
+  },
+  {
+    "name": "野莓萊姆冰沙",
+    "calories": 360
+  },
+  {
+    "name": "冰巧克力(大)",
+    "calories": 360
+  },
+  {
+    "name": "葡萄柚萊姆冰沙",
+    "calories": 404
+  },
+  {
+    "name": "蘋果漾芒果萊姆冰沙",
+    "calories": 419
+  },
+  {
+    "name": "蘋果漾野莓萊姆冰沙",
+    "calories": 457
+  },
+  {
+    "name": "蘋果漾葡萄柚萊姆冰沙",
+    "calories": 463
+  }
+];
+
+var coffeeInf = [{
+    "name": "特選黑咖啡",
+    "calories": 0
+  },
+  {
+    "name": "義式濃縮",
+    "calories": 0
+  },
+  {
+    "name": "冰特選黑咖啡",
+    "calories": 0
+  }, {
+    "name": "經典美式咖啡",
+    "calories": 6
+  }, {
+    "name": "熱咖啡",
+    "calories": 7
+  },
+  {
+    "name": "特選重黑咖啡",
+    "calories": 17
+  },
+  {
+    "name": "特選卡布奇諾(大)",
+    "calories": 183
+  },
+  {
+    "name": "摩卡(大)",
+    "calories": 184
+  },
+  {
+    "name": "冰摩卡(大)",
+    "calories": 299
+  },
+  {
+    "name": "冰特選那堤(大)",
+    "calories": 187
+  },
+  {
+    "name": "特濃白咖啡(大)",
+    "calories": 196
+  },
+  {
+    "name": "特選那堤(大)",
+    "calories": 197
+  },
+  {
+    "name": "冰香草那堤(大)",
+    "calories": 208
+  },
+  {
+    "name": "冰焦糖瑪琪朵(大)",
+    "calories": 215
+  }, {
+    "name": "香草那堤(大)",
+    "calories": 286
+  }
+]
+
+//早餐
+$("#selectBreakfast").on("click", () => {
+  $("#normalOptions").hide();
+  $("#normalOptions svg").remove();
+  $("#snackOptions").hide();
+  $("#snackOptions svg").remove();
+  $("#drinkOptions").hide();
+  $("#drinkOptions svg").remove();
+  $("#coffeeOptions").hide();
+  $("#coffeeOptions svg").remove();
+
+  $("#breakfastOptions").append(`<div id="color">
+    <p><span id="light"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 小於300大卡</span> </p>
+    <p><span id="medium"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 300~500大卡</span> </p>
+    <p><span id="fat"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 大於500大卡</span> </p>
+  </div>`);
+
+  const s1 = d3.select("#breakfastOptions").append("svg").attr("width", "75vw").attr("height", "90vh");
+  const g1 = s1.selectAll("g").data(breakfastInf).enter().append("g");
+  const scale1 = d3.scaleLinear().domain([0, 800]).range([0, 800]);
+
+  g1.append('rect')
+    .attr('x', 180)
+    .attr('y', (item, i) => i * 30)
+    .attr('width', 0)
+    .attr('height', 25)
+    .style('fill', (item) => {
+      if (item.calories > 300 && item.calories <= 500) {
+        return "#ED784A";
+      } else if (item.calories > 500) {
+        return "#C73E3A";
+      } else {
+        return "#91B493";
+      }
+    })
+    .transition()
+    .duration(1000)
+    .attr('width', (item) => {
+      return item.calories;
+    });
+
+  g1.append('text')
+    .text((item) => item.name)
+    .attr("x", 0)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+  g1.append('text')
+    .text((item) => {
+      return 0;
+    })
+    .attr("x", 183)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+    .transition()
+    .duration(1000)
+    .attr("x", (item) => item.calories + 183)
+    .tween('number', (item) => {
+      var i = d3.interpolateRound(0, item.calories);
+      return function(t) {
+        this.textContent = i(t)
+      };
+    });
+
+  const axis1 = d3
+    .axisBottom(scale1)
+    .ticks(10)
+    .tickFormat(item => item + 'kcal');
+  let axis1Y = breakfastInf.length * 30 + 18;
+  s1.append('g').call(axis1).attr('transform', `translate(180,${axis1Y})`);
+  $("#breakfastOptions").show();
+})
+
+//一般主餐
+$("#selectNormal").on("click", () => {
+  $("#breakfastOptions").hide();
+  $("#breakfastOptions svg").remove();
+  $("#snackOptions").hide();
+  $("#snackOptions svg").remove();
+  $("#drinkOptions").hide();
+  $("#drinkOptions svg").remove();
+  $("#coffeeOptions").hide();
+  $("#coffeeOptions svg").remove();
+
+  $("#normalOptions").append(`<div id="color">
+    <p><span id="light"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 小於300大卡</span> </p>
+    <p><span id="medium"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 300~500大卡</span> </p>
+    <p><span id="fat"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 大於500大卡</span> </p>
+  </div>`);
+
+  const s2 = d3.select("#normalOptions").append("svg").attr("width", "75vw").attr("height", "130vh");
+  const g2 = s2.selectAll("g").data(normalInf).enter().append("g");
+  const scale2 = d3.scaleLinear().domain([0, 600]).range([0, 600]);
+  g2.append('rect')
+    .attr('x', 180)
+    .attr('y', (item, i) => i * 30)
+    .attr('width', 0)
+    .attr('height', 25)
+    .style('fill', (item) => {
+      if (item.calories > 300 && item.calories <= 500) {
+        return "#ED784A";
+      } else if (item.calories > 500) {
+        return "#C73E3A";
+      } else {
+        return "#91B493";
+      }
+    }).transition()
+    .duration(1000)
+    .attr('width', (item) => {
+      return item.calories;
+    });
+  g2.append('text')
+    .text((item) => item.name)
+    .attr("x", 0)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+  g2.append('text')
+    .text((item) => {
+      return 0;
+    })
+    .attr("x", 183)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+    .transition()
+    .duration(1000)
+    .attr("x", (item) => item.calories + 183)
+    .tween('number', (item) => {
+      var i = d3.interpolateRound(0, item.calories);
+      return function(t) {
+        this.textContent = i(t)
+      };
+    });
+  const axis2 = d3
+    .axisBottom(scale2)
+    .ticks(6)
+    .tickFormat(item => item + 'kcal');
+  let axis2Y = normalInf.length * 30 + 18;
+  s2.append('g').call(axis2).attr('transform', `translate(180,${axis2Y})`);
+  $("#normalOptions").show();
+})
+
+//點心
+$("#selectSnack").on("click", () => {
+  $("#breakfastOptions").hide();
+  $("#breakfastOptions svg").remove();
+  $("#normalOptions").hide();
+  $("#normalOptions svg").remove();
+  $("#drinkOptions").hide();
+  $("#drinkOptions svg").remove();
+  $("#coffeeOptions").hide();
+  $("#coffeeOptions svg").remove();
+
+  $("#snackOptions").append(`<div id="color">
+    <p><span id="light"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 小於300大卡</span> </p>
+    <p><span id="medium"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 300~500大卡</span> </p>
+    <p><span id="fat"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 大於500大卡</span> </p>
+  </div>`);
+
+  const s3 = d3.select("#snackOptions").append("svg").attr("width", "75vw").attr("height", "80vh");
+  const g3 = s3.selectAll("g").data(snackInf).enter().append("g");
+  const scale3 = d3.scaleLinear().domain([0, 400]).range([0, 400]);
+  g3.append('rect')
+    .attr('x', 180)
+    .attr('y', (item, i) => i * 30)
+    .attr('width', 0)
+    .attr('height', 25)
+    .style('fill', (item) => {
+      if (item.calories > 300 && item.calories <= 500) {
+        return "#ED784A";
+      } else if (item.calories > 500) {
+        return "#C73E3A";
+      } else {
+        return "#91B493";
+      }
+    }).transition()
+    .duration(1000)
+    .attr('width', (item) => {
+      return item.calories;
+    });
+  g3.append('text')
+    .text((item) => item.name)
+    .attr("x", 0)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+  g3.append('text')
+    .text((item) => {
+      return 0;
+    })
+    .attr("x", 183)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+    .transition()
+    .duration(1000)
+    .attr("x", (item) => item.calories + 183)
+    .tween('number', (item) => {
+      var i = d3.interpolateRound(0, item.calories);
+      return function(t) {
+        this.textContent = i(t)
+      };
+    });
+  const axis3 = d3
+    .axisBottom(scale3)
+    .ticks(4)
+    .tickFormat(item => item + 'kcal');
+  let axis3Y = snackInf.length * 30 + 18;
+  s3.append('g').call(axis3).attr('transform', `translate(180,${axis3Y})`);
+  $("#snackOptions").show();
+})
+
+//飲料
+$("#selectDrink").on("click", () => {
+  $("#breakfastOptions").hide();
+  $("#breakfastOptions svg").remove();
+  $("#normalOptions").hide();
+  $("#normalOptions svg").remove();
+  $("#snackOptions").hide();
+  $("#snackOptions svg").remove();
+  $("#coffeeOptions").hide();
+  $("#coffeeOptions svg").remove();
+
+  $("#drinkOptions").append(`<div id="color">
+    <p><span id="light"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 小於300大卡</span> </p>
+    <p><span id="medium"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 300~500大卡</span> </p>
+    <p><span id="fat"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 大於500大卡</span> </p>
+  </div>`);
+
+  const s4 = d3.select("#drinkOptions").append("svg").attr("width", "75vw").attr("height", "140vh");
+  const g4 = s4.selectAll("g").data(drinkInf).enter().append("g");
+  const scale4 = d3.scaleLinear().domain([0, 500]).range([0, 500]);
+  g4.append('rect')
+    .attr('x', 250)
+    .attr('y', (item, i) => i * 30)
+    .attr('width', 0)
+    .attr('height', 25)
+    .style('fill', (item) => {
+      if (item.calories > 300 && item.calories <= 500) {
+        return "#ED784A";
+      } else if (item.calories > 500) {
+        return "#C73E3A";
+      } else {
+        return "#91B493";
+      }
+    }).transition()
+    .duration(1000)
+    .attr('width', (item) => {
+      return item.calories;
+    });
+  g4.append('text')
+    .text((item) => item.name)
+    .attr("x", 0)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+  g4.append('text')
+    .text((item) => {
+      return 0;
+    })
+    .attr("x", 253)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+    .transition()
+    .duration(1000)
+    .attr("x", (item) => item.calories + 253)
+    .tween('number', (item) => {
+      var i = d3.interpolateRound(0, item.calories);
+      return function(t) {
+        this.textContent = i(t)
+      };
+    });
+  const axis4 = d3
+    .axisBottom(scale4)
+    .ticks(5)
+    .tickFormat(item => item + 'kcal');
+  let axis4Y = drinkInf.length * 30 + 18;
+  s4.append('g').call(axis4).attr('transform', `translate(250,${axis4Y})`);
+  $("#drinkOptions").show();
+})
+
+//咖啡
+$("#selectCoffee").on("click", () => {
+  $("#breakfastOptions").hide();
+  $("#breakfastOptions svg").remove();
+  $("#normalOptions").hide();
+  $("#normalOptions svg").remove();
+  $("#snackOptions").hide();
+  $("#snackOptions svg").remove();
+  $("#drinkOptions").hide();
+  $("#drinkOptions svg").remove();
+
+  $("#coffeeOptions").append(`<div id="color">
+    <p><span id="light"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 小於300大卡</span> </p>
+    <p><span id="medium"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 300~500大卡</span> </p>
+    <p><span id="fat"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span> 大於500大卡</span> </p>
+  </div>`);
+
+  const s5 = d3.select("#coffeeOptions").append("svg").attr("width", "75vw").attr("height", "70vh");
+  const g5 = s5.selectAll("g").data(coffeeInf).enter().append("g");
+  const scale5 = d3.scaleLinear().domain([0, 300]).range([0, 300]);
+  g5.append('rect')
+    .attr('x', 180)
+    .attr('y', (item, i) => i * 30)
+    .attr('width', 0)
+    .attr('height', 25)
+    .style('fill', (item) => {
+      if (item.calories > 300 && item.calories <= 500) {
+        return "#ED784A";
+      } else if (item.calories > 500) {
+        return "#C73E3A";
+      } else {
+        return "#91B493";
+      }
+    }).transition()
+    .duration(1000)
+    .attr('width', (item) => {
+      return item.calories;
+    });
+  g5.append('text')
+    .text((item) => item.name)
+    .attr("x", 0)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+  g5.append('text')
+    .text((item) => {
+      return 0;
+    })
+    .attr("x", 183)
+    .attr('y', (item, i) => i * 30 + 18)
+    .style('fill', 'black')
+    .style('font-size', '18px')
+    .transition()
+    .duration(1000)
+    .attr("x", (item) => item.calories + 183)
+    .tween('number', (item) => {
+      var i = d3.interpolateRound(0, item.calories);
+      return function(t) {
+        this.textContent = i(t)
+      };
+    });
+  const axis5 = d3
+    .axisBottom(scale5)
+    .ticks(3)
+    .tickFormat(item => item + 'kcal');
+  let axis5Y = coffeeInf.length * 30 + 18;
+  s5.append('g').call(axis5).attr('transform', `translate(180,${axis5Y})`);
+
+  $("#coffeeOptions").show();
+})
